74338db527a39a9df629f2f0b97e4c1b55ced6aa
Trịnh Ngọc Sơn <77819889+trinhngcson@users.noreply.github.com>
2023-04-10 01:46:23 +0700

    api v2

--- backend/controller/userCtrl.js
+++ backend/controller/userCtrl.js
@@ -1,6 +1,8 @@
 const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
 //register
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
@@ -40,6 +42,7 @@ const getallUser = asyncHandler(async (req, res) => {
 //get single user
 const getaUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  validateMongoDbId(id);
   try {
     const getaUser = await User.findById(id);
     res.json({ getaUser });
@@ -47,4 +50,81 @@ const getaUser = asyncHandler(async (req, res) => {
     throw new Error(error);
   }
 });
-module.exports = { createUser, loginUserCtrl, getallUser, getaUser };
+//update a user
+const updatedUser = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  validateMongoDbId(_id);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        name: req.body.name,
+        email: req.body.email,
+        mobile: req.body.mobile,
+      },
+      { new: true }
+    );
+    res.json({
+      updatedUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+//deleta user
+const deleteaUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deleteaUser = await User.findByIdAndDelete(id);
+    res.json({ deleteaUser });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const blockUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const block = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: true,
+      },
+      { new: true }
+    );
+    res.json({
+      msg: "Tài khoản đã bị khoá",
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const unblockUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const unblock = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: false,
+      },
+      { new: true }
+    );
+    res.json({
+      msg: "Tài khoản đã mở khoá",
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = {
+  createUser,
+  loginUserCtrl,
+  getallUser,
+  getaUser,
+  deleteaUser,
+  updatedUser,
+  blockUser,
+  unblockUser,
+};
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -4,13 +4,22 @@ const {
   loginUserCtrl,
   getallUser,
   getaUser,
+  deleteaUser,
+  updatedUser,
+  blockUser,
+  unblockUser,
 } = require("../controller/userCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getallUser);
-router.get("/:id", getaUser);
+router.get("/:id", authMiddleware, isAdmin, getaUser);
+router.delete("/:id", deleteaUser);
+router.put("/edit-user", authMiddleware, updatedUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
 
 module.exports = router;
